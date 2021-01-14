fcf8293da62a468cf824a9760275c9a2efbdc844
Shoji Imamura <m-imamura@goldkey.co.jp>
2021-01-14 15:27:37 +0900

    add files

---
+++ function.go
@@ -0,0 +1,35 @@
+// Package p contains an HTTP Cloud Function.
+package p
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"io"
+	"log"
+	"net/http"
+)
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Message string `json:"message"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		switch err {
+		case io.EOF:
+			fmt.Fprint(w, "aaaaaa")
+			return
+		default:
+			log.Printf("json.NewDecoder: %v", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+	}
+
+	if d.Message == "" {
+		fmt.Fprint(w, "Hello World!")
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(d.Message))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nc30/cloudfunction_ci
+
+go 1.13
